15425e1392b05644f05f4aaa06b9cf03988a8604
Aleksey Smirnov <debugger94@gmail.com>
2022-03-12 14:40:15 +0300

    Rework thrusters

--- common/screen_wrap.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@onready var parent_node := get_parent() as Node2D
-@onready var viewport_rect := get_viewport().get_visible_rect()
-
-func _process(_delta: float) -> void:
-	parent_node.position.x = wrapf(parent_node.position.x, 0, viewport_rect.size.x)
-	parent_node.position.y = wrapf(parent_node.position.y, 0, viewport_rect.size.y)
--- player/player.gd
+++ player/player.gd
@@ -1,21 +1,26 @@
 extends RigidDynamicBody2D
 
 @export var thrust_force: float = 300
-@export var turn_force: float = 300
+@export var turn_force: float = 400
 
 @onready var viewport_rect := get_viewport().get_visible_rect()
-@onready var thruster := $Thruster as Thruster
+@onready var thruster_left  := $Thrusters/ThrusterLeft  as Thruster
+@onready var thruster_right := $Thrusters/ThrusterRight as Thruster
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("move_forward"):
-		thruster.active = true
+		thruster_left.active  = true
+		thruster_right.active = true
 		var forward_direction = Vector2.UP.rotated(rotation)
 		apply_force(forward_direction * thrust_force)
 	else:
-		thruster.active = false
+		thruster_left.active  = false
+		thruster_right.active = false
 	
 	var turn_direction := Input.get_axis("turn_left", "turn_right")
 	if turn_direction != 0:
+		thruster_left.active  = turn_direction > 0
+		thruster_right.active = turn_direction < 0
 		apply_torque(turn_direction * turn_force)
 		
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
