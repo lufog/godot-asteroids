bf55a35d26e3ffe670959f8d769a2461b9c0f0de
Aleksey Smirnov <debugger94@gmail.com>
2022-03-26 19:52:17 +0300

    Add direction deviation to debris spawner

--- spawners/debris_spawner/debris spawner.gd
+++ spawners/debris_spawner/debris spawner.gd
@@ -5,13 +5,24 @@ class_name DebrisSpawner extends Node2D
 @export var offset := 10.0
 @export var force := 50.0
 
+const random_rotation := 4.0
+const random_direction_deviation := 0.5
+
 @onready var asteroids_node := $/root/Main/Asteroids as Node
+
+
+func _calculate_deviated_direction(direction_index: int) -> Vector2:
+	var deviation := Vector2(
+		randf_range(-random_direction_deviation, random_direction_deviation),
+		randf_range(-random_direction_deviation, random_direction_deviation)
+	)
+	return directions[direction_index] + deviation
 
 
 func spawn() -> void:
 	for i in directions.size():
 		var debris = debris_scene.instantiate() as Asteroid
 		debris.global_position = global_position + (directions[i] * offset)
-		debris.angular_velocity = randf_range(-4, 4)
-		debris.linear_velocity = directions[i] * force
+		debris.angular_velocity = randf_range(-random_rotation, random_rotation)
+		debris.linear_velocity = _calculate_deviated_direction(i) * force
 		asteroids_node.add_child.call_deferred(debris)
