2e912f5ccaa1514428f2d0d872cf507512f67164
Aleksey Smirnov <debugger94@gmail.com>
2022-04-18 20:45:27 +0300

    Save/load settings file

--- global/settings.gd
+++ global/settings.gd
@@ -3,6 +3,8 @@ extends Node
 
 enum _BusIdx { MASTER = 0, MUSIC = 1, SOUND = 2 }
 
+const settings_file_path = "user://settings.dat"
+
 var _master_volume: float
 var master_volume:
 	get:
@@ -29,15 +31,29 @@ var sound_volume:
 
 
 func _ready() -> void:
-	master_volume = 1
-	music_volume = 1
-	sound_volume = 1
 	load_from_file()
 
 
 func load_from_file() -> void:
-	print("load_settings")
+	var file := File.new()
+	if file.open(settings_file_path, File.READ) == OK:
+		# File opened, load data from file
+		master_volume = file.get_float()
+		music_volume = file.get_float()
+		sound_volume = file.get_float()
+		file.close()
+	else:
+		# Failed to open file, set defaults
+		master_volume = 1.0
+		music_volume = 1.0
+		sound_volume = 1.0
 
 
 func save_to_file() -> void:
-	print("save_settings")
+	var file := File.new()
+	if file.open(settings_file_path, File.WRITE) == OK:
+		# File opened, save data to file
+		file.store_float(master_volume)
+		file.store_float(music_volume)
+		file.store_float(sound_volume)
+		file.close()
